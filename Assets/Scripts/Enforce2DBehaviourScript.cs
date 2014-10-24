d2a8dd91e0e45ebf7a37cec68a23caaf88b2fa7c
Valtis <erkka.kaaria@helsinki.fi>
2014-10-24 12:55:05 +0300

    Changed Enforce2DBehaviour to use constraints instead of dirty hack

--- Assets/Scripts/Enforce2DBehaviourScript.cs
+++ Assets/Scripts/Enforce2DBehaviourScript.cs
@@ -5,12 +5,13 @@ public class Enforce2DBehaviourScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		if (rigidbody != null) {
+			rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezePositionZ;
+		}
+
 	}
 
 	// enforce 2d behaviour as due to joint limitations we are forced to use 3d
 	void Update () {
-		transform.position = new Vector3 (transform.position.x, transform.position.y, 0);
-		transform.localEulerAngles = new Vector3 (0, 0, transform.localEulerAngles.z);
 	}
 }
