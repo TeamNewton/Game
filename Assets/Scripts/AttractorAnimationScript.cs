3bbc431f3225dba97950524b07abe98a0dc7ff19
Ville-Matti Tanninen <ville-matti.tanninen@outlook.com>
2014-10-26 17:48:38 +0200

    Added visual effects for pushing and pulling objects.

---
+++ Assets/Scripts/AttractorAnimationScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttractorAnimationScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		gameObject.transform.localPosition = new Vector3 (0, 0, 0);
+
+		Color originalColor = renderer.material.color;
+		gameObject.transform.localScale = new Vector3 (2, 2, 1);
+		renderer.material.color = new Color(originalColor.r, originalColor.g, originalColor.b, 0.0f);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Color originalColor = renderer.material.color;
+		if (gameObject.transform.localScale.x <= 0.1f) 
+		{
+			gameObject.transform.localScale = new Vector3 (2, 2, 1);
+			renderer.material.color = new Color(originalColor.r, originalColor.g, originalColor.b, 0.0f);
+		} 
+		else 
+		{
+			gameObject.transform.localScale -= new Vector3 (1.0f*Time.deltaTime, 1.0f*Time.deltaTime, 0);
+			renderer.material.color = new Color(originalColor.r, originalColor.g, originalColor.b, originalColor.a + 0.15f*Time.deltaTime);
+		}
+	}
+}
---
+++ Assets/Scripts/pushAnimationScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pushAnimationScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		gameObject.transform.localPosition = new Vector3 (0, 0, 0);
+
+		Color originalColor = renderer.material.color;
+		gameObject.transform.localScale = new Vector3 (0.1f, 0.1f, 1);
+		renderer.material.color = new Color(originalColor.r, originalColor.g, originalColor.b, 0.3f);
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Color originalColor = renderer.material.color;
+		if (gameObject.transform.localScale.x >= 2) 
+		{
+			gameObject.transform.localScale = new Vector3 (0.1f, 0.1f, 1);
+			renderer.material.color = new Color(originalColor.r, originalColor.g, originalColor.b, 0.3f);
+		} 
+		else 
+		{
+			gameObject.transform.localScale += new Vector3 (1.0f*Time.deltaTime, 1.0f*Time.deltaTime, 0);
+			renderer.material.color = new Color(originalColor.r, originalColor.g, originalColor.b, originalColor.a - 0.15f*Time.deltaTime);
+		}
+	}
+}
